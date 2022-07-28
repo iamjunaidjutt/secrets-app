5a8d8ae9db75b7cdda6bcb92114e24d9b7b50cfa
iamjunaidjutt <engr.muhammadjunaid@hotmail.com>
2022-07-29 01:08:19 +0500

    install and setup mongodb npm module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,7 +9,31 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb://localhost:27017/userDB", (err) => {
+    if (err) console.log(err.message);
+    else {
+        console.log("Connected to MongoDB");
+    }
+});
 
+const userSchema = mongoose.Schema({
+    email: String,
+    passward: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
 
 
 
